0ccecc62574e5e3fca8073e793f7eae6e22d7ee6
Cássio Zen <cassiozen@gmail.com>
2015-10-19 15:52:09 -0200

    Moved to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151019161110) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authorizations", force: :cascade do |t|
     t.string   "key"
     t.datetime "created_at", null: false
